b4be1d96d3ad04662badd2f599506fd4b7a4ea19
Roman Stashchak <stashchak90@gmail.com>
2020-12-19 21:19:34 +0200

    les 6_1 in progress

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import React, {Component} from 'react';
+import User from "./components/User";
 
 class App extends Component {
-myForm=React.createRef();
-state={users:[], inputValue:''};
-componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(value => value.json())
-        .then(value=> this.setState({users:value}))
-}
+    myForm = React.createRef();
+    state = {inputValue: '', users: [], flag: ''};
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => this.setState({users: value}))
+    }
 
     render() {
-    let {users, inputValue}=this.state;
+        let {users, inputValue, flag} = this.state;
         return (
-           <div>
-            <form action={'/savedata'} onSubmit={this.save} ref={this.myForm}>
-                <input value={this.state.inputValue} type={'number'} onInput={this.commitState}></input>
-                <button>save</button>
-            </form>
-               <div>
-                   {inputValue users.find(value => value.id===+inputValue)}
+            <div>
 
-               </div>
-           </div>
+                <form action={'/savedata'} onSubmit={this.save} ref={this.myForm}>
+                    <input value={this.state.inputValue} type='number' onInput={this.commitState}/>
+                    <button>Show User</button>
+                </form>
+
+                <div>
+                    {flag && <User item={users.find(value => value.id === +flag)} key={inputValue}/>}
+
+                </div>
+            </div>
         );
     }
+
+    commitState = (e) => {
+        this.setState({inputValue: e.target.value});
+    }
+    save=(e)=>{
+        e.preventDefault();
+        this.setState({flag: e.target[0].value});
+
+    }
+
+
 }
 
 export default App;
--- src/components/User.js
+++ src/components/User.js
@@ -2,7 +2,27 @@ export default function User (props){
     const {item}=props;
     return (
         <div>
-            {item.id} - {item.name}
+            {item &&
+            <div>
+                <p><b>id: </b>{item.id}</p>
+                <p><b>name: </b>{item.name}</p>
+                <p><b>username: </b>{item.username}</p>
+                <p><b>email: </b>{item.email}</p>
+                <p><b>street: </b>{item.address.street}</p>
+                <p><b>suite: </b>{item.address.suite}</p>
+                <p><b>city: </b>{item.address.city}</p>
+                <p><b>zipcode: </b>{item.address.zipcode}</p>
+                <p><b>geo-lat: </b>{item.address.geo.lat}</p>
+                <p><b>geo-lng: </b>{item.address.geo.lng}</p>
+
+            </div>
+            }
+            {!item &&
+            <div>
+                No data for view!!!
+            </div>
+            }
+
         </div>
     );
 }
