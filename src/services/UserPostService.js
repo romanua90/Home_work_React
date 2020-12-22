f089bcea06c5d9fe47327005d38be72f86d26e69
Roman Stashchak <stashchak90@gmail.com>
2020-12-22 20:21:32 +0200

    les 6_1 in progress

---
+++ src/services/UserPostService.js
@@ -0,0 +1,11 @@
+export default class UserPostService extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default UserPostService;
