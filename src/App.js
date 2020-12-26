8a2204b76b31fcac442397e3b4e00450b52765bb
Roman Stashchak <stashchak90@gmail.com>
2020-12-26 19:22:54 +0200

    les 7_2 finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React, { useState, useEffect, useReducer} from 'react';
 
+const reducer=(state, action)=>{
+    switch (action.type){
+        case "SET_USER":{
+            return action.payload;
+        }
+        case "CHANGE_USER_ID":{
+            return {
+                ...state,
+                id: action.payload
+            };
+        }
+        case "CHANGE_USER_NAME":{
+            return {
+                ...state,
+                name: action.payload
+            };
+        }
+        default:{
+            console.log('Not found case for action!')
+            return state;
+        }
+    }
+}
 
 export default function App() {
-
+    const initialState={
+        id: null,
+        name: null,
+        username:null,
+    }
+   const [state, dispatch]=useReducer(reducer,initialState);
     const [user, setUser] = useState();
     const [counter, setCounter] = useState(1)
 
@@ -10,7 +38,7 @@ export default function App() {
            fetch(`https://jsonplaceholder.typicode.com/users/${counter}`)
                 .then(response => response.json())
                 .then((json)=> {
-                  setUser(json);
+                  dispatch({type:"SET_USER", payload:json});
             });
 
         },
@@ -25,24 +53,29 @@ export default function App() {
 
     }
     const onClickNameHandler = () => {
-        setUser((prevState) => ({...prevState, name: prevState.name + '!'}))
+        dispatch({type:'CHANGE_USER_NAME', payload: state.name.toUpperCase()+'____Changed' })
+    }
+    const onClickIdHandler = ()=>{
+        dispatch({type:'CHANGE_USER_ID', payload: Math.floor(Math.random()*500)})
     }
     return (
         <div>
             <h1>Counter: {counter}</h1>
-            {!!user &&
+            {!!state &&
             (
                 <div>
-                    <h3>User id: {user.id}</h3>
-                    <h3>User name: {user.name}</h3>
-                    <h3>User username: {user.username}</h3>
-                    <h3>User city: {user.address.city}</h3>
+                    <h3>User id: {state.id}</h3>
+                    <h3>User name: {state.name}</h3>
+                    <h3>User username: {state.username}</h3>
+                    {/*<h3>User city: {state.address.city}</h3>*/}
                 </div>
             )
             }
             <button onClick={onClickIncHandler}>Increment</button>
             <button onClick={onClickDecHandler}>Decrement</button>
             <button onClick={onClickNameHandler}>Change User name</button>
+            <button onClick={onClickIdHandler}>Change User id</button>
+
 
 
         </div>
